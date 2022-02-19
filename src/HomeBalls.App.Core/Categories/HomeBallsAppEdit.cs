497740cb9819b09275f000fdb3b679a7a128b45a
blah12629 <eo.cullano@gmail.com>
2022-02-19 08:26:40 +0800

    Added icons

--- src/HomeBalls.App.Core/Categories/HomeBallsAppEdit.cs
+++ src/HomeBalls.App.Core/Categories/HomeBallsAppEdit.cs
@@ -11,5 +11,8 @@ public class HomeBallsAppEdit :
 {
     public HomeBallsAppEdit(ILogger? logger = default) : base(default, logger) { }
 
-    protected internal override void GenerateIconSvgPaths(List<String> paths) { }
+    protected internal override void GenerateIconSvgPaths(List<String> paths)
+    {
+        paths.Add("M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z");
+    }
 }
