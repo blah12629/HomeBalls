7207d26c437178cb5abd3a302e6c13632da6dc0c
blah12629 <30469126+blah12629@users.noreply.github.com>
2022-03-25 15:02:49 +0000

    Deploying to gh-pages from @ blah12629/HomeBalls@7fd89d6e6242cbfde3349e381cf6cdf80312bcce 🚀

--- _content/HomeBalls.App.Components/app.settings.js
+++ _content/HomeBalls.App.Components/app.settings.js
@@ -1,8 +1,10 @@
 function openComponent(outerId, innerId) {
+    console.log(`closeComponent(${outerId}, ${innerId})`);
     let height = document.getElementById(innerId).getBoundingClientRect().height;
     document.getElementById(outerId).style.height = `${parseFloat(height)}px`;
 }
 
 function closeComponent(outerId) {
+    console.log(`closeComponent(${outerId})`);
     document.getElementById(outerId).style.height = "0px";
 }
